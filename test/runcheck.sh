340f9c0f22609d687a016dd653ba3df0e3f8a202
Kavindu Gimhan Zoysa <kavindu175@gmail.com>
2021-08-19 14:58:55 +0530

    Add the initial test

--- test/runcheck.sh
+++ test/runcheck.sh
@@ -11,6 +11,16 @@ b=`basename "$1"`
 kind=`echo "$b" | head -c 1`
 out="out/actual/$b.txt"
 mkdir -p out/actual
+
+BAL_FILE=$(echo "$1" | sed -e s/"out\/result"/"..\/compiler\/testSuite"/ -e s/exe/bal/)
+HEAP_SIZE=$(sed -ne 's;^.*// *@small_heap  *;;p' "$BAL_FILE")
+
+if [ ! -z "$HEAP_SIZE" ]
+then
+    export SMALL_HEAP=$HEAP_SIZE
+fi
+export LD_LIBRARY_PATH=../runtime/gc-roots/third-party-lib/libbacktrace/.libs
+
 if test $kind == P; then
     $("./$1" >/dev/null 2>"$out")
 else
